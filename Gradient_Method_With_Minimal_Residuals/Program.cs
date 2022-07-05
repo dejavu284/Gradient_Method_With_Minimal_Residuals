059fcfb9ef7f317250678aa89884a9ca1eadd565
dejavu284 <skuratov.1@mail.ru>
2022-07-05 16:57:09 +0700

    Добавил возможность решения СЛАУ любого размера, не только 2 на 2. Автоматизировал все выводы на консоль. Добавил тестовые примеры. Сделал user friendly интерфейс, на сколько это возможно в консоли)), осталось добавить задержку на вывод текста на консоль, чтоб всё было не так резко

--- Gradient_Method_With_Minimal_Residuals/Program.cs
+++ Gradient_Method_With_Minimal_Residuals/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Gradient_Method_With_Minimal_Residuals
 {
@@ -44,7 +45,7 @@ namespace Gradient_Method_With_Minimal_Residuals
             double[,] Matrix_A = null;
             switch (count)
             {
-                case 1:
+                case 2:
                     double[,] Matrix_1 =
                     {
                         { 33, 1},
@@ -52,20 +53,12 @@ namespace Gradient_Method_With_Minimal_Residuals
                     };
                     Matrix_A = Matrix_1;
                     break;
-                case 2:
-                    double[,] Matrix_2 =
-                    {
-                        { 10, 11},
-                        { 11, 11}
-                    };
-                    Matrix_A = Matrix_2;
-                    break;
                 case 3:
                     double[,] Matrix_3 =
                     {
                         { 33, 1, 2},
                         { 1, 11, 3},
-                        { 2, 3, 12}
+                        { 2, 3, 11}
                     };
                     Matrix_A = Matrix_3;
                     break;
@@ -96,10 +89,38 @@ namespace Gradient_Method_With_Minimal_Residuals
 
             for (int i = 0; i < Vector_B.Length; i++)
             {
+                Console.WriteLine("Элемент с индексом: {0}", i);
                 Vector_B[i] = double.Parse(Console.ReadLine());
             }
             return Vector_B;
         }//пользовательский
+
+        static double[] GetDefaultVector(int count)
+        {
+            double[] Vector = null;
+            switch (count)
+            {
+                case 1:
+                    double[] Vector_1 = { 34, 12 };
+                    Vector = Vector_1;
+                    break;
+                case 2:
+                    double[] Vector_2 = { 1, 1.1 };
+                    Vector = Vector_2;
+                    break;
+                case 3:
+                    double[] Vector_3 = { 34, 12, 23};
+                    Vector = Vector_3;
+                    break;
+                case 4:
+                    double[] Vector_4 = { 1, 1.1, 1.2 };
+                    Vector = Vector_4;
+                    break;
+                default:
+                    break;
+            }
+            return Vector;
+        }
 
         static void PrintMatrix<T>(T[,] Matrix)
         {
@@ -125,7 +146,7 @@ namespace Gradient_Method_With_Minimal_Residuals
 
         static double[] MatrixOnVector(double [,] Matrix, double[] Vector)
         {
-            double[] AX = new double[2];
+            double[] AX = new double[Vector.Length];
 
             for (int i = 0; i < Matrix.GetLength(0); i++)
             {
@@ -136,24 +157,30 @@ namespace Gradient_Method_With_Minimal_Residuals
                 }
             }
             return AX;
-        }//матрицу на вектор
+        }//матрицу на вектор 
 
         static double[] Vector_Subtraction_Vector(double[] Vector_first, double[] Vector_second)
         {
-            double[] R = new double[2];
-            R[0] = Vector_first[0] - Vector_second[0];
-            R[1] = Vector_first[1] - Vector_second[1];
+            double[] R = new double[Vector_first.Length];
+            for (int i = 0; i < Vector_first.Length; i++)
+            {
+                R[i] = Vector_first[i] - Vector_second[i];
+            }
+            
             return R;
         }
 
         static double Dot_Product(double[] Vector_first, double[] Vector_second)
         {
-            double[] NewVec = new double[2];
+            double[] NewVec = new double[Vector_first.Length];
 
-            NewVec[0] += Vector_first[0] * Vector_second[0];
-            NewVec[1] += Vector_first[1] * Vector_second[1];
+            double sum = 0;
 
-            double sum = NewVec[0] + NewVec[1];
+            for (int i = 0; i < Vector_first.Length; i++)
+            {
+                NewVec[i] += Vector_first[i] * Vector_second[i];
+                sum += NewVec[i];
+            }
 
             return sum;
 
@@ -170,15 +197,22 @@ namespace Gradient_Method_With_Minimal_Residuals
                     double[] Vector_X = null;
                     Console.WriteLine("Начальные данные:\n");
 
-                    Console.WriteLine("Выберете количество линейных уравнений в слау:");
+                    Console.WriteLine("Выберете количество линейных уравнений в СЛАУ:\nДопустимые значения[2,10]");
                     int size = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Ок.\n");
+                    if(size >=2 && size<=10) Console.WriteLine("Ок.\n");
+                    else
+                    {
+                        Console.WriteLine("Недопустимое значение, начни сначала...");
+                        continue;
+                    }
+
 
                     Console.WriteLine("Матрица A:");
+
                     bool flag1 = true;
                     while (flag1)
                     {
-                        Console.WriteLine("Выберите способ создания матрицы А:\n1 - Ввод с клавиатуры\n2 - Создание рандомной матрицы\n3 - Создание стандартной матрицы\n");
+                        Console.WriteLine("Выберите способ создания матрицы А:\n1 - Ввод с клавиатуры\n2 - Создание рандомной матрицы\n3 - Создание матрицы для тестового примера\n");
                         Console.Write(": ");
                         int otvet = int.Parse(Console.ReadLine());
                         switch (otvet)
@@ -198,7 +232,14 @@ namespace Gradient_Method_With_Minimal_Residuals
                                 break;
 
                             case 3:
-                                Matrix_A = GetDefaultMatrix_A(1); // Возвращает стандартную матрицу А, есть несколько стандартных матриц, возращает матрицу по номеру
+                                Console.WriteLine("Есть 2 стандартных матрицы А, размерностью 2 и 3.");
+                                Console.WriteLine("Будет создана матрица размерностью {0}",size);
+                                if (size == 2 || size == 3) Matrix_A = GetDefaultMatrix_A(size); // Возвращает матрицу А для тестового примера
+                                else
+                                {
+                                    Console.WriteLine("Под такую размерность нет тестовых примеров.");
+                                    continue;
+                                }
                                 flag1 = false;
                                 break;
 
@@ -213,36 +254,134 @@ namespace Gradient_Method_With_Minimal_Residuals
 
                     Console.WriteLine("Вектор B:");
 
-                    //Vector_B = GetUserVector(size); // Возвращает введённый пользователем вектор В
+                    bool flag2 = true;
+                    while (flag2)
+                    {
+                        Console.WriteLine("Выберите способ создания вектора B:\n1 - Ввод с клавиатуры\n2 - Создание рандомного вектора\n3 - Создание вектора для тестового примера\n");
+                        Console.Write(": ");
+                        int otvet = int.Parse(Console.ReadLine());
+                        switch (otvet)
+                        {
+                            case 1:
+                                Vector_B = GetUserVector(size); // Возвращает введённый пользователем вектор В
+                                flag2 = false;
+                                break;
 
-                    //Vector_B = GetRandomVector(size, 10.2, 20.2); // Возвращает рандомный вектор В
+                            case 2:
+                                Console.WriteLine("Введите диапазон значений(от - до)\nОт:");
+                                double first = double.Parse(Console.ReadLine());
+                                Console.WriteLine("До:");
+                                double second = double.Parse(Console.ReadLine());
+                                Vector_B = GetRandomVector(size, first, second); // Возвращает рандомный вектор В выбранного размера, с диапазоном значений
+                                flag2 = false;
+                                break;
 
-                    //Vector_B = { 34, 12 }; // Создаёт стандартный вектор В
+                            case 3:
+                                Console.WriteLine("Есть 2 стандартных вектора B, размерностью 2 и 3.");
+                                Console.WriteLine("Будет создан вектор подходящего под СЛАУ размера");
+                                if (size == 2)
+                                {
+                                    Vector_B = GetDefaultVector(1); // Возвращает вектор В для тестового примера
+                                }
+                                else if (size == 3)
+                                {
+                                    Vector_B = GetDefaultVector(3); // Возвращает вектор В для тестового примера
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Под такую размерность нет тестовых примеров.");
+                                    continue;
+                                }
+                                flag2 = false;
+                                break;
+
+                            default:
+                                Console.WriteLine("Введён некоректный символ, попробуй еще раз...");
+                                break;
+                        }
+                    }
 
                     PrintVector(Vector_B); // Вывести вектор В на экран
 
 
                     Console.WriteLine("Вектор X:");
-                    //Vector_X = GetUserVector(); // Возвращает введённый пользователем вектор X
+                    bool flag3 = true;
+                    while (flag3)
+                    {
+                        Console.WriteLine("Выберите способ создания вектора X:\n1 - Ввод с клавиатуры\n2 - Создание рандомного вектора\n3 - Создание вектора для тестового примера\n");
+                        Console.Write(": ");
+                        int otvet = int.Parse(Console.ReadLine());
+                        switch (otvet)
+                        {
+                            case 1:
+                                Vector_X = GetUserVector(size); // Возвращает введённый пользователем вектор X
+                                flag3 = false;
+                                break;
 
-                    //Vector_X = GetRandomVector(); // Возвращает рандомный вектор X
+                            case 2:
+                                Console.WriteLine("Введите диапазон значений(от - до)\nОт:");
+                                double first = double.Parse(Console.ReadLine());
+                                Console.WriteLine("До:");
+                                double second = double.Parse(Console.ReadLine());
+                                Vector_X = GetRandomVector(size, first, second); // Возвращает рандомный вектор X выбранного размера, с диапазоном значений
+                                flag3 = false;
+                                break;
 
-                    //Vector_X = { 1, 1.1 }; // Задали значения X
+                            case 3:
+                                Console.WriteLine("Есть 2 стандартных вектора X, размерностью 2 и 3.");
+                                Console.WriteLine("Будет создан вектор подходящего под СЛАУ размера");
+                                if (size == 2)
+                                {
+                                    Vector_X = GetDefaultVector(2); // Возвращает вектор X для тестового примера
+                                }
+                                else if (size == 3)
+                                {
+                                    Vector_X = GetDefaultVector(4); // Возвращает вектор X для тестового примера
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Под такую размерность нет тестовых примеров.");
+                                    continue;
+                                }
+                                flag3 = false;
+                                break;
+
+                            default:
+                                Console.WriteLine("Введён некоректный символ, попробуй еще раз...");
+                                break;
+                        }
+                    }
 
                     PrintVector(Vector_X); // Вывести вектор X на экран 
 
+                    Console.WriteLine("В итоге начальные данные такие...\n");
 
-                    double Old_X0 = 0;
-                    double Old_X1 = 0;
+                    Console.WriteLine("Матрица A:");
+                    PrintMatrix(Matrix_A);
+
+                    Console.WriteLine("Вектор B:");
+                    PrintVector(Vector_B);
+
+                    Console.WriteLine("Вектор X:");
+                    PrintVector(Vector_X);
+
+
+                    double[] Old_X = new double[size];
                     bool flag = false;
                     int count = 0;
+                    Console.ReadKey(true);
                     while (count < 100)
                     {
                         Console.WriteLine("//===========================================================================//");
                         Console.WriteLine("//=============================== Шаг - {0} ===================================//", count);
                         Console.WriteLine("//===========================================================================//\n");
 
-                        Console.WriteLine("X:\n  Х1 = {0,5:0.000}\n  X2 = {1,5:0.000}\n", Vector_X?[0], Vector_X?[1]);
+                        Console.WriteLine("X:");
+                        for (int i = 0; i < Vector_X?.Length; i++)
+                        {
+                            Console.Write("Х{0} = {1,5:0.000}\n", i, Vector_X?[i]);
+                        }
+                        Console.WriteLine();
 
                         double[] AX = MatrixOnVector(Matrix_A, Vector_X);// Считаем AX
                         Console.WriteLine("AX:");
@@ -262,25 +401,43 @@ namespace Gradient_Method_With_Minimal_Residuals
                         double AR_AR = Dot_Product(AR, AR);// Считаем AR_AR
                         Console.WriteLine("(AR,AR):\n{0,7:0.00}\n", AR_AR);
 
-                        double alpha = AR_R / AR_AR;// Считаем 
+                        double alpha;
+                        if (AR_AR != 0) alpha = AR_R / AR_AR;// Считаем альфа
+                        else
+                        {
+                            Console.WriteLine("(AR,AR) = 0, делить на ноль нельзя, попробуйте другие начальные данные...");
+                            continue;
+                        }
                         Console.WriteLine("alpha:\n{0,8:0.0000}\n", alpha);
 
-                        Old_X0 = Vector_X[0];// Сохраняем старые X
-                        Old_X1 = Vector_X[1];
-                        Console.WriteLine("Старые:\nХ1 = {0,5:0.000}\nX2 = {1,5:0.000}\n", Old_X0, Old_X1);
+                        Console.WriteLine("Старые:");
 
-                        Vector_X[0] = (Vector_X[0] + (alpha * R[0]));// Считаем новые X
-                        Vector_X[1] = (Vector_X[1] + (alpha * R[1]));
-                        Console.WriteLine("Новые:\nХ1 = {0,5:0.000}\nX2 = {1,5:0.000}\n", Vector_X[0], Vector_X[1]);
+                        for (int i = 0; i < size; i++)
+                        {
+                            Old_X[i] = Vector_X[i];// Сохраняем старые X
+                            Console.WriteLine("Х{0} = {1,5:0.000}", i, Old_X[i]);
+                        }
+                        Console.WriteLine();
 
-                        double[] Eps_arr = new double[2];
+                        Console.WriteLine("Новые:");
+                        for (int i = 0; i < Vector_X?.Length; i++)
+                        {
+                            Vector_X[i] = (Vector_X[i] + (alpha * R[i]));// Считаем новые X
+                            Console.Write("Х{0} = {1,5:0.000}\n", i, Vector_X?[i]);
+                        }
+                        Console.WriteLine();
 
-                        Eps_arr[0] = Math.Abs(Old_X0 - Vector_X[0]);
-                        Console.WriteLine("Эпсилон_1: {0,6:0.0000}", Eps_arr[0]);
-                        Eps_arr[1] = Math.Abs(Old_X1 - Vector_X[1]);
-                        Console.WriteLine("Эпсилон_2: {0,6:0.0000}", Eps_arr[1]);
+                        double[] Eps_arr = new double[size];
 
-                        double e = Math.Max(Eps_arr[0], Eps_arr[1]);// Считаем Епсилон
+                        for (int i = 0; i < Eps_arr.Length; i++)
+                        {
+                            Eps_arr[i] = Math.Abs(Old_X[i] - Vector_X[i]);
+                            Console.WriteLine("Эпсилон_{0}: {1,6:0.0000}",i, Eps_arr[i]);
+                        }
+                        Console.WriteLine();
+                        
+                        double e = Eps_arr.Max();// Считаем Епсилон
+
                         Console.WriteLine("Эпсилон max:\n{0,8:0.0000}\n", e);
 
                         count++;
@@ -293,7 +450,11 @@ namespace Gradient_Method_With_Minimal_Residuals
 
                     if (flag)
                     {
-                        Console.WriteLine("Окончательный ответ Х1 = {0,5:0.000}, X2 = {1,5:0.000}", Old_X0, Old_X1);
+                        Console.WriteLine("Окончательный ответ:");
+                        for (int i = 0; i < size; i++)
+                        {
+                            Console.WriteLine("Х{0} = {1,5:0.000}", i, Old_X[i]);
+                        }
                     }
 
                     else Console.WriteLine("Программа зациклилась, попробуйте другий значения");
